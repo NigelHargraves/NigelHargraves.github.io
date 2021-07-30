5bd50ea8d6324f3dd5fbf300e19524dce22d0564
Nigel Hargraves <nigelhargraves@ymail.com>
2021-07-30 21:36:40 +0100

    changed buttons

---
+++ js/dist/Space Invaders.dev.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var gunPos = 10;
+var moveLeft = false;
+var moveRight = false;
+
+function checkKey(e) {
+  if (e.keyCode == 37 || e.keyCode == 65) {
+    moveLeft = true;
+  } else if (e.keyCode == 39 || e.keyCode == 68) {
+    moveRight = true;
+  }
+}
+
+function movePlayer() {
+  if (moveLeft == true && gunPos > 10) {
+    gunPos -= 0.5;
+    gun.style.left = gunPos + "%";
+  } else if (gunPos < 10) {
+    gunPos = 10;
+  }
+
+  if (moveRight == true && gunPos < 90) {
+    gunPos += 0.5;
+    gun.style.left = gunPos + "%";
+  } else if (gunPos > 90) {
+    gunPos = 90;
+  }
+}
+
+function moveInvader() {}
+
+function stopKey() {
+  moveLeft = false;
+  moveRight = false;
+}
+
+document.onkeydown = checkKey;
+document.onkeyup = stopKey;
+setInterval(movePlayer, 50);
+setInterval(moveInvader, 50);
