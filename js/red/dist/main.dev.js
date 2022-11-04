a68b12b369edd034333ae81e2303010891bc5b49
Nigel Hargraves <nigelhargraves@ymail.com>
2022-11-04 18:23:55 +0000

    new game start (red)

---
+++ js/red/dist/main.dev.js
@@ -0,0 +1,243 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+// Set the canvas element to a variable.
+var ctx = c.getContext("2d");
+c.width = window.innerWidth;
+c.height = window.innerHeight;
+var ctx2 = c2.getContext("2d");
+c2.width = window.innerWidth;
+var layers = [];
+var KP = {}; //Keyspressed array
+
+var KR = {}; //Keysreleased array
+
+var moveLeft = false,
+    moveRight = false,
+    jump = false,
+    sit = false;
+var gravity = 0.03,
+    friction = 0.006,
+    velocityAmount = 0.02,
+    groundPosition = 677;
+var background1 = new Image();
+background1.src = "https://img.freepik.com/free-vector/cartoon-nature-landscape-with-mountain-forest-deciduous-trees-trunks-clearance_107791-3706.jpg?w=1380&t=st=1664548409~exp=1664549009~hmac=1db2c723702f96a80b9fca1dbb0063bdbc4b05541455b5950ff9d2e649ceee37";
+var background2 = new Image();
+background2.src = "https://img.freepik.com/free-photo/top-view-bright-green-grass-texture-background_93675-134844.jpg?w=1380&t=st=1664550289~exp=1664550889~hmac=44ed39c3e4ec6343139f783c716acaf925366e8e8871506a4fcdf7cba28fd09b";
+var playerImage = new Image();
+playerImage.src = 'images/red/player/Idle1.png'; //create layer class.
+
+var Layer =
+/*#__PURE__*/
+function () {
+  //construct layer data.
+  function Layer(image, x, y, height, speed) {
+    _classCallCheck(this, Layer);
+
+    this.x = x;
+    this.y = y;
+    this.width = 6000;
+    this.height = height;
+    this.x2 = this.width;
+    this.image = image;
+    this.speed = speed;
+  } //draw layer.
+
+
+  _createClass(Layer, [{
+    key: "draw",
+    value: function draw() {
+      if (this.image == background1) {
+        ctx.drawImage(this.image, this.x, this.y + (groundPosition - player.y), this.width, this.height);
+        ctx.drawImage(this.image, this.x2, this.y + (groundPosition - player.y), this.width, this.height);
+      } else {
+        ctx2.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx2.drawImage(this.image, this.x2, this.y, this.width, this.height);
+      }
+
+      c2.style.top = groundPosition + 100 + (groundPosition - player.y) + "px";
+    } //update layer.
+
+  }, {
+    key: "update",
+    value: function update() {
+      if (this.image == background1) {
+        this.speed = player.velocity.x;
+      } else {
+        this.speed = player.velocity.x * 1.25;
+      }
+
+      if (player.velocity.x >= 0) {
+        if (this.x <= -this.width) {
+          this.x = this.width;
+        }
+
+        if (this.x2 <= -this.width) {
+          this.x2 = this.width;
+        }
+      } else {
+        if (this.x >= this.width) {
+          this.x = -this.width;
+        }
+
+        if (this.x2 >= this.width) {
+          this.x2 = -this.width;
+        }
+      }
+
+      this.x -= this.speed;
+      this.x2 -= this.speed;
+      this.draw();
+    }
+  }]);
+
+  return Layer;
+}(); //create player class.
+
+
+var Player =
+/*#__PURE__*/
+function () {
+  //construct player data.
+  function Player(x, y) {
+    _classCallCheck(this, Player);
+
+    this.x = x;
+    this.y = y;
+    this.velocity = {
+      x: 0,
+      y: 0
+    };
+  } //draw player.
+
+
+  _createClass(Player, [{
+    key: "draw",
+    value: function draw() {
+      ctx.drawImage(playerImage, this.x, this.y, 100, 100);
+    }
+  }, {
+    key: "update",
+    value: function update() {
+      if (this.velocity.y > 0) {
+        this.velocity.y -= friction;
+      } else {
+        this.velocity.y += friction;
+      }
+
+      if (this.velocity.x > 0) {
+        this.velocity.x -= friction;
+      } else {
+        this.velocity.x += friction;
+      }
+
+      if (this.y > groundPosition - 1) {
+        if (moveLeft) {
+          this.velocity.x -= velocityAmount;
+        } else if (moveRight) {
+          this.velocity.x += velocityAmount;
+        }
+      }
+
+      if (jump) {
+        if (this.y > groundPosition - 1) {
+          this.velocity.y = -2;
+        }
+      } else if (sit) {
+        if (this.y > groundPosition - 1) {
+          if (this.velocity.x > 0.3 || this.velocity.x < -0.3) {
+            if (this.velocity.x < 0) {
+              this.velocity.x += 0.1;
+            } else {
+              this.velocity.x -= 0.1;
+            }
+          } else {
+            this.velocity.x = 0;
+          }
+        }
+      } //update position.
+
+
+      this.x += this.velocity.x;
+      this.y += this.velocity.y; //add gravity.
+
+      if (this.y < groundPosition) {
+        this.velocity.y += gravity;
+      } else {
+        this.velocity.y = 0;
+        this.y = groundPosition;
+      }
+
+      this.draw();
+    }
+  }]);
+
+  return Player;
+}();
+
+function init() {
+  player = new Player(c.width / 2, c.height - 100);
+  layers.push(new Layer(background1, 0, -c.height, c.height * 2, 0));
+  layers.push(new Layer(background2, 0, 0, c2.height, 0));
+}
+
+function animate() {
+  //call next frame.
+  animationId = requestAnimationFrame(animate);
+  layers.forEach(function (layer, index) {
+    layer.update();
+  });
+  player.update();
+  ctx.font = "20px Arial";
+  ctx.fillStyle = "white";
+  ctx.fillText("This.y = " + player.y, 0, 20);
+} //adjust canvas on screen resize.
+
+
+window.addEventListener("resize", function () {
+  c.width = window.innerWidth;
+  c.height = window.innerHeight;
+  init();
+});
+window.addEventListener("keydown", function (e) {
+  if (e.keyCode == 37 || e.keyCode == 65) {
+    moveLeft = true;
+  }
+
+  if (e.keyCode == 39 || e.keyCode == 68) {
+    moveRight = true;
+  }
+
+  if (e.keyCode == 83 || e.keyCode == 40) {
+    sit = true;
+  }
+
+  if (e.keyCode == 87 || e.keyCode == 38) {
+    jump = true;
+  }
+});
+window.addEventListener("keyup", function (e) {
+  if (e.keyCode == 37 || e.keyCode == 65) {
+    moveLeft = false;
+  }
+
+  if (e.keyCode == 39 || e.keyCode == 68) {
+    moveRight = false;
+  }
+
+  if (e.keyCode == 83 || e.keyCode == 40) {
+    sit = false;
+    player.style.background = "red";
+  }
+
+  if (e.keyCode == 87 || e.keyCode == 38) {
+    jump = false;
+  }
+});
+init();
+animate();
