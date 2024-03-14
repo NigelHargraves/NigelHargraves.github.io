0eff65197d89efe5fd5381b63d60fee7238b28a6
Nigel Hargraves <nigelhargraves@ymail.com>
2024-03-14 11:07:15 +0000

    update

--- js/Guitar/Lagrima/dist/strings.dev.js
+++ js/Guitar/Lagrima/dist/strings.dev.js
@@ -43,93 +43,39 @@ function forStrings() {
 }
 
 function noteToDisplay(gap) {
-  var minusNote = 0;
-
-  if (nextNote == 0) {
-    minusNote = 0;
-  } else {
-    minusNote = 1;
-  }
-
   var num;
   var string;
 
-  if (noteToPlay[nextNote - minusNote] == CO1) {
-    num = 17;
-    string = strings[1].y;
-  }
-
-  if (noteToPlay[nextNote - minusNote] == DO1) {
+  if (!DO1.paused) {
     num = 20;
     string = strings[2].y;
+    ctx.beginPath();
+    ctx.arc(fretBoard.x + fretNumber[num].pos, fretBoard.y + gap + string, 5, 0, Math.PI * 2);
+    ctx.fillStyle = 'white';
+    ctx.fill();
   }
 
-  if (noteToPlay[nextNote - minusNote] == EO1) {
-    num = 18;
-    string = strings[2].y;
-  }
-
-  if (noteToPlay[nextNote - minusNote] == FO1) {
+  if (!FO1.paused) {
     num = 17;
     string = strings[2].y;
+    ctx.beginPath();
+    ctx.arc(fretBoard.x + fretNumber[num].pos, fretBoard.y + gap + string, 5, 0, Math.PI * 2);
+    ctx.fillStyle = 'white';
+    ctx.fill();
   }
 
-  if (noteToPlay[nextNote - minusNote] == GO1) {
-    num = 20;
-    string = strings[3].y;
-  }
-
-  if (noteToPlay[nextNote - minusNote] == AO1) {
+  if (!AO1.paused) {
     num = 18;
     string = strings[3].y;
+    ctx.beginPath();
+    ctx.arc(fretBoard.x + fretNumber[num].pos, fretBoard.y + gap + string, 5, 0, Math.PI * 2);
+    ctx.fillStyle = 'white';
+    ctx.fill();
   }
 
-  if (noteToPlay[nextNote - minusNote] == BO1) {
-    num = 20;
-    string = strings[4].y;
-  }
-
-  if (noteToPlay[nextNote - minusNote] == CO2) {
+  if (!CO2.paused) {
     num = 19;
     string = strings[4].y;
-  }
-
-  if (noteToPlay[nextNote - minusNote] == DO2) {
-    num = 17;
-    string = strings[4].y;
-  }
-
-  if (noteToPlay[nextNote - minusNote] == EO2) {
-    num = 20;
-    string = strings[5].y;
-  }
-
-  if (noteToPlay[nextNote - minusNote] == FO2) {
-    num = 19;
-    string = strings[5].y;
-  }
-
-  if (noteToPlay[nextNote - minusNote] == GO2) {
-    num = 17;
-    string = strings[5].y;
-  }
-
-  if (noteToPlay[nextNote - minusNote] == AO2) {
-    num = 15;
-    string = strings[5].y;
-  }
-
-  if (noteToPlay[nextNote - minusNote] == BO2) {
-    num = 13;
-    string = strings[5].y;
-  }
-
-  if (noteToPlay[nextNote - minusNote] == CO3) {
-    num = 12;
-    string = strings[5].y;
-  }
-
-  if (!noteToPlay[nextNote].ended) {
     ctx.beginPath();
     ctx.arc(fretBoard.x + fretNumber[num].pos, fretBoard.y + gap + string, 5, 0, Math.PI * 2);
     ctx.fillStyle = 'white';
